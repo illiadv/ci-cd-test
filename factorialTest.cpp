af4bbfbf8b780fda0c4d5d6f8ac61b3b367f033c
illiadv <illiadanylov.5@gmail.com>
2026-05-21 17:30:25 +0200

    Handle user exception

--- factorialTest.cpp
+++ factorialTest.cpp
@@ -1,5 +1,6 @@
 #include "factorial.hpp"
 #include "gtest/gtest.h"
+#include "user_exception.hpp"
 
 TEST(factorialTest, HandlesZeroInput)
 {
@@ -16,7 +17,7 @@ TEST(factorialTest, HandlesPositiveInput)
 
 TEST(factorialTest, HandleNegativeInput)
 {
-    EXPECT_EQ(factorial(-10), -1);
+    EXPECT_THROW(factorial(-10), UserException);
 }
 
 int main(int argc, char **argv)
