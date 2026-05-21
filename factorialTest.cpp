37509131537076395a3bedadb951bd2fc56642ec
illiadv <illiadanylov.5@gmail.com>
2026-05-21 16:56:23 +0200

    Add GTest tests

---
+++ factorialTest.cpp
@@ -0,0 +1,21 @@
+#include "factorial.hpp"
+#include "gtest/gtest.h"
+
+TEST(factorialTest, HandlesZeroInput)
+{
+    EXPECT_EQ(factorial(0), 1);
+}
+
+TEST(factorialTest, HandlesPositiveInput)
+{
+    EXPECT_EQ(factorial(1), 1);
+    EXPECT_EQ(factorial(2), 2);
+    EXPECT_EQ(factorial(3), 6);
+    EXPECT_EQ(factorial(8), 40320);
+}
+
+int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
